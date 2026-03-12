7c8cb55dc54033d8aeb75be5da0578fb2a84b550
Arun Kumar Meda <medaarun390@gmail.com>
2026-03-13 00:31:50 +0530

    Added connection between frontend and backend in api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import { auth } from "../firebase/config";
 
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://dsce-mini-project.onrender.com";
 
 
 // =================================
