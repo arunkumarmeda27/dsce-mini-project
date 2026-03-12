630910f3bc74f9a672b3853b781784290288ee60
Arun Kumar Meda <medaarun390@gmail.com>
2026-03-13 00:19:19 +0530

    Added requirements.txt for Render deploy

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAL_CwVTF7c93JMFdy_q57PRRl7z3NwJ7U",
-    authDomain: "dsce-mini-project.firebaseapp.com",
-    projectId: "dsce-mini-project",
-    storageBucket: "dsce-mini-project.firebasestorage.app",
-    messagingSenderId: "609164741519",
-    appId: "1:609164741519:web:67a91c4f7fae54459d99fa",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
